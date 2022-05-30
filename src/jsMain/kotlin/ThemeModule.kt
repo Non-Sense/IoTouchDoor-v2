b4511ff05906d2e9a447e164e6cdc555f8645055
N0nSense <yu200029@gmail.com>
2022-05-31 02:41:43 +0900

    add: theme mode saving

--- src/jsMain/kotlin/ThemeModule.kt
+++ src/jsMain/kotlin/ThemeModule.kt
@@ -1,3 +1,4 @@
+import cookie.Cookies
 import mui.material.CssBaseline
 import mui.material.styles.Theme
 import mui.material.styles.ThemeProvider
@@ -9,8 +10,10 @@ typealias ThemeState = StateInstance<Theme>
 val ThemeContext = createContext<ThemeState>()
 
 val ThemeModule = FC<PropsWithChildren> { props ->
-    val state = useState(Themes.Light)
+    val state = useState(if(Cookies.get("dk")?.toBoolean() == true) Themes.Dark else Themes.Light)
     val theme by state
+
+    Cookies.set("dk", (theme == Themes.Dark).toString())
 
     ThemeContext.Provider(state) {
         ThemeProvider {
