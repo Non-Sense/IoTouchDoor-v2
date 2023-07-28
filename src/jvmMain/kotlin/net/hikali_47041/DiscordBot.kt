0c976fc63bdfd9fb1dade7b3b37b1eda26a88ea7
N0nSense <yu200029@gmail.com>
2023-07-28 16:08:49 +0900

    mod: add timestamp to bot's text message

--- src/jvmMain/kotlin/net/hikali_47041/DiscordBot.kt
+++ src/jvmMain/kotlin/net/hikali_47041/DiscordBot.kt
@@ -1,6 +1,9 @@
 package net.hikali_47041
 
 import net.dv8tion.jda.api.JDABuilder
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class DiscordBot(
     discordBotToken: String,
@@ -8,13 +11,19 @@ class DiscordBot(
 ) {
     private val jda = JDABuilder.createDefault(discordBotToken).build()
 
+    companion object {
+        private val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSS")
+    }
+
     fun start() {
         jda.awaitReady()
     }
 
     fun sendNotify() {
         jda.getTextChannelById(channelId)?.apply {
-            sendMessage("Ping").queue()
+            val time = LocalDateTime.now(ZoneId.of("Asia/Tokyo"))
+            val formattedTime = formatter.format(time)
+            sendMessage("Ping at $formattedTime").queue()
         }
     }
 }
